d3d15b1378ed48699ce7a8c2d8a82dc8d37e63ae
GianniGiubel <131212993+GianniGiubel@users.noreply.github.com>
2023-08-30 19:01:48 -0300

    criei exe 87

---
+++ aula87_historico_redirecionamento/script.js
@@ -0,0 +1,10 @@
+const url=document.querySelector('#url')
+const btn_url=document.querySelector('#btn_url')
+
+btn_url.addEventListener("click", (evento)=> {
+    // window.location="https://www.google.com.br"
+    // window.location.replace("https://www.google.com.br") // retira do histórico a url anterior
+    // window.location.assign("https://www.google.com.br") // Não remove do histórico
+    // window.location.reload() // Recarrega a página
+    // window.history // métodos do histórico
+})

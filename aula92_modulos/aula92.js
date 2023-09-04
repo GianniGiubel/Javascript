c71c49553c65415bf602824ef6474f27d8b605b5
GianniGiubel <131212993+GianniGiubel@users.noreply.github.com>
2023-09-04 19:08:10 -0300

    assiti aula de módulos

---
+++ aula92_modulos/aula92.js
@@ -0,0 +1,10 @@
+import getTodosCursos from "./cursos.js"
+
+// import getTodosCursos , { cursos as c , getCurso as gc } from "./cursos.js";
+
+import * as m_cursos from "./cursos.js"
+
+
+console.log(m_cursos.default())
+console.log(m_cursos.cursos)
+console.log(m_cursos.getCurso(2))
---
+++ aula92_modulos/cursos.js
@@ -0,0 +1,15 @@
+const cursos=["javascrript","HTML","CSS","Arduino","Raspberry","C++","Python","java","C#"]
+
+// const getTodosCursos = () => {
+//     return cursos
+// }
+
+export default function getTodosCursos() {
+    return cursos
+}
+
+function getCurso(i_curso) {
+    return cursos[i_curso]
+}
+
+export {cursos,getCurso}

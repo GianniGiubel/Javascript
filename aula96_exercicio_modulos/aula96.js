018be6d62be1ed8146422d8eca4cd4b2a9df0f7d
GianniGiubel <131212993+GianniGiubel@users.noreply.github.com>
2023-09-06 21:13:19 -0300

    adicionei botao de remoção

--- aula96_exercicio_modulos/aula96.js
+++ aula96_exercicio_modulos/aula96.js
@@ -3,12 +3,18 @@ import c from "./contatos.js"
 const listaContatos=document.querySelector('#listaContatos')
 const btn_gravar=document.querySelector('#btn_gravar')
 
-btn_gravar.addEventListener("click", (evento)=>{
+btn_gravar.addEventListener("click", (event)=>{    
     const cont={
         nome:document.querySelector('#f_nome').value,
         telefone:document.querySelector('#f_telefone').value,
         email:document.querySelector('#f_email').value
-    }
-    c.addContato(cont,listaContatos)
-    console.log(c.getTodosContatos())
+    }    
+    // if(cont.nome == "" || cont.telefone == "" || cont.email == "") {
+    //     alert("Por Favor, preencha todos os campos")
+    // } else {
+        c.addContato(cont,listaContatos)
+        // console.log(c.getTodosContatos())
+        // console.log(c.getContato(0).nome)
+    // }
+    
 })
--- aula96_exercicio_modulos/contatos.js
+++ aula96_exercicio_modulos/contatos.js
@@ -14,18 +14,31 @@ let contato={
             email:novoContato.email
         }
         contatos.push(cont)
-        const div=document.createElement("div")
-        div.setAttribute("class","contato")
-        const p_nome=document.createElement("p")
-        p_nome.innerHTML=novoContato.nome
-        const p_telefone=document.createElement("p")
-        p_telefone.innerHTML=novoContato.telefone
-        const p_email=document.createElement("p")
-        p_email.innerHTML=novoContato.email
-        div.appendChild(p_nome)
-        div.appendChild(p_telefone)
-        div.appendChild(p_email)
-        destinoDOM.appendChild(div)
+        destinoDOM.innerHTML=""
+
+        contatos.forEach((elemento)=> {
+            const div=document.createElement("div")
+            div.setAttribute("class","contato")
+            const p_nome=document.createElement("p")
+            p_nome.innerHTML=elemento.nome
+            const p_telefone=document.createElement("p")
+            p_telefone.innerHTML=elemento.telefone
+            const p_email=document.createElement("p")
+            p_email.innerHTML=elemento.email
+            const btn_remover=document.createElement("button")
+            btn_remover.setAttribute("id", "btn_" + indice)
+            btn_remover.innerHTML="Remover"
+            div.appendChild(p_nome)
+            div.appendChild(p_telefone)
+            div.appendChild(p_email)
+            div.appendChild(btn_remover)
+            destinoDOM.appendChild(div)
+            btn_remover.addEventListener("click",(event)=> {
+                event.target.parentNode.remove()
+                contatos.pop(elemento)
+                console.log(contatos)
+            })            
+        })        
     }
 }
 

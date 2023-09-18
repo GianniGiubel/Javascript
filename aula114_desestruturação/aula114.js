4e5c10b31eff9e16239d80429f08a59eda83c774
GianniGiubel <131212993+GianniGiubel@users.noreply.github.com>
2023-09-18 19:03:00 -0300

    aula114 desestruturação

---
+++ aula114_desestruturação/aula114.js
@@ -0,0 +1,20 @@
+// let a,b,c,d
+
+// [a,b=0,c=0,d=0]=[10]
+
+// let numeros=[10,20,30,40]
+// let [a,b,c,d]=numeros
+
+// [a,b,c,d]=[10,20,30,40]
+
+// ({a,b,c,d}={a:"verde",b:"amarelo",c:"azul",d:"branco"})
+
+let a=10;
+let b=20;
+
+[a,b]=[b,a]
+
+console.log(a)
+console.log(b)
+// console.log(c)
+// console.log(d)

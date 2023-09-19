ea221b3a4eff1fbb0342d0f4962478b370718de2
GianniGiubel <131212993+GianniGiubel@users.noreply.github.com>
2023-09-19 19:37:35 -0300

    terminei aula 120

--- aula120_validacao_nativa_2/aula120.js
+++ aula120_validacao_nativa_2/aula120.js
@@ -3,13 +3,18 @@ const f_nota=document.querySelector('#f_nota')
 const f_msg=document.querySelector('#f_msg')
 
 document.querySelector('#btn_validar').addEventListener("click",(event)=> {
-    let msg=null
+    let estadoValidacao=f_nota.validity
 
-    if(!f_nota.checkValidity()) {
-        msg=f_nota.validationMessage
+    if(estadoValidacao.valueMissing) {
+        f_nota.setCustomValidity("Este campo é obrigatório!")        
+    } else if (estadoValidacao.rangeOverflow) {
+        f_nota.setCustomValidity("Preencha com sua nota de 0 até 10")
+    } else if (estadoValidacao.rangeUnderflow) {
+        f_nota.setCustomValidity("Preencha com sua nota de 0 até 10")
     }
 
-    f_msg.innerHTML=msg
+    f_nota.reportValidity()
+    f_msg.innerHTML=f_nota.validationMessage
     event.preventDefault()
     })
 
@@ -27,8 +32,8 @@ document.querySelector('#btn_validar').addEventListener("click",(event)=> {
 
 // customError: true, se uma mensagem de validação personalizada for definida.
 // patternMissmatch: true, se o valor de um elemento não corresponder ao seu atributo padrão.
-// rangeOverFlow: true, se o valor de um elemento for maior que seu atributo max.
-// rangeUnderFlow: true, se o valor de um elemento for menor que seu atributo min.
+// rangeOverflow: true, se o valor de um elemento for maior que seu atributo max.
+// rangeUnderflow: true, se o valor de um elemento for menor que seu atributo min.
 // setpMissmatch: true, se o valor de um elemento for inválido por seu atributo step.
 // tooLong: true, se o valor de um elemento exceder seu atributo maxLenght.
 // typeMissmatch: true, se o valor de um elemento for inválido por seu atributo type.
